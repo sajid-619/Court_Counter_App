4c120ca214e7dea3994dcfa478a52e5a4cc6b864
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-06-29 17:20:22 +0600

    Update MainActivity.java

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,19 +1,24 @@
-package com.example.android.courtcounter;
+package com.sagarxm.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends ActionBarActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         displayForTeamA(0);
+        displayForTeamB(0);
     }
 
     @Override
@@ -37,52 +42,78 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public int scoreA = 0;
-    public int scoreB = 0;
-    /**
-     * Displays the given score for Team A.
-     */
+
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
-    public void plus3(View view){
-        scoreA = scoreA + 3;
-        displayForTeamA(score);
-    }
-    public void plus2(View view){
-        scoreA = scoreA + 2;
-        displayForTeamA(scoreA);
-    }
-    public void freethrow(View view){
-        scoreA = scoreA + 1;
-        displayForTeamA(scoreA);
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 
-    //for team B
-    public void plus3b(View view){
-        scoreB = scoreB + 3;
-        displayForTeamB(scoreB);
-    }
-    public void plus2b(View view){
-        scoreB = scoreB + 2;
-        displayForTeamB(scoreB);
-    }
-    public void freethrowb(View view){
-        scoreB = scoreB + 1;
-        displayForTeamB(scoreB);
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
     }
 
-    public void reset(View view){
-        scoreA=0;
-        scoreB=0;
-        displayForTeamA(scoreA);
-        displayForTeamB(scoreB);
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 1 points.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team B by 1 points.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
+
+  
